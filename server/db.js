e6cf483cd528f688d8bbb39421f63887558cf3db
arnoldsouth <arnoldsouth@gmail.com>
2023-10-25 01:05:21 -0700

    update favicon

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
-const Pool = require("pg").Pool;
+const Pool = require('pg').Pool;
 
 const pool = new Pool({
-  host: "localhost",
-  user: "postgres",
-  password: "kthl8822",
+  host: 'localhost',
+  user: 'postgres',
+  password: 'qweasdzxc',
   port: 5432,
-  database: "authtodolist"
+  database: 'authtodolist',
 });
 
 module.exports = pool;
